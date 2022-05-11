505aba4c928f0d1dacfb496668c426255c8c79ea
Rashmi Singh <rashmi.virtualgain5@gmail.com>
2022-05-12 00:40:52 +0530

    Few more test cases

--- tdd/src/test/java/com/tddexample/AddTest.java
+++ tdd/src/test/java/com/tddexample/AddTest.java
@@ -24,4 +24,22 @@ public class AddTest {
 		assertEquals("The method should return 0. ",expected, actual);
 	}
 
+	@Test
+	public void testSingleNumAdd() {
+		assertEquals("The method should return the entered value. ", 5, addnum.addnumbers("5"));
+	}
+	
+	@Test
+	public void testUnknownNumAdd() {
+		int expected = 22;
+		int actual = addnum.addnumbers("1,2,3,5,1,10");
+		assertEquals("The method should add all numbers in the input string", expected, actual);
+	}
+	
+	@Test
+	public void testNewlineBetweenNum() {
+		int expected = 6;
+		int actual = addnum.addnumbers("1\n2,3");
+		assertEquals("The method should handle new line in between numbers", expected, actual);
+	}
 }
